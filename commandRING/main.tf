6b98a5f27568a5061dadec34ed6356ed20b81e63
Jacek Kuśnierz <jaku@grapeup.com>
2020-03-11 16:08:49 +0100

    test docker

---
+++ commandRING/main.tf
@@ -0,0 +1,12 @@
+provider "docker" {
+  host = "ssh://jenkins@145.239.88.134:22"
+}
+
+resource "docker_container" "worker" {
+  name  = "sample_worker"
+  image = "${docker_image.worker.latest}"
+}
+
+resource "docker_image" "worker" {
+  name = "worker_image:latest"
+}
